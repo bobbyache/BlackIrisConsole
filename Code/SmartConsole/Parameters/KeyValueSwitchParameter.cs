6041e182fbc0edb46dd72e3f35b699fde6f41b37
bobyache <robbie.a.blake@gmail.com>
2016-05-09 16:56:11 +0200

    removed unwanted hasSwitch code

--- Code/SmartConsole/Parameters/KeyValueSwitchParameter.cs
+++ Code/SmartConsole/Parameters/KeyValueSwitchParameter.cs
@@ -8,36 +8,15 @@ namespace BlackIris.Parameters
 {
     internal class KeyValueSwitchParameter : ISwitchParameter
     {
-        public readonly bool HasSwitch;
         public readonly string Text;
-
         public string Switch { get; private set; }
+        //public char SwitchPrefix { get; private set; }
 
         internal KeyValueSwitchParameter(string argument, string containedSwitch)
         {
-            this.HasSwitch = hasSwitch(argument);
-
-            if (this.HasSwitch)
-            {
-                this.Switch = argument.Substring(0, containedSwitch.Length);
-                this.Text = argument.Substring(containedSwitch.Length, argument.Length - (containedSwitch.Length));
-            }
-            else
-            {
-                this.Switch = "";
-                this.Text = argument;
-            }
-        }
-
-        private bool hasSwitch(string argument)
-        {
-            if (argument.Count() == 0)
-                return false;
-
-            if (argument[0] == '-' || argument[0] == '/')
-                return true;
-            else
-                return false;
+            this.Switch = argument.Substring(0, containedSwitch.Length);
+            this.Text = argument.Substring(containedSwitch.Length, argument.Length - (containedSwitch.Length));
+            //this.SwitchPrefix = char.Parse(containedSwitch.Substring(0, 1));
         }
     }
 }
